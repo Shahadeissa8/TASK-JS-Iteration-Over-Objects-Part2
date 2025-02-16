ba0ba33ed7ee07cefe7fdf58475b51bf9310c524
Shahadeissa8 <shahadeissa98@gmail.com>
2025-02-16 15:25:08 +0300

    sol

--- objectIteration.js
+++ objectIteration.js
@@ -53,45 +53,73 @@ const students = [
 
 // 1) Write a `getStudentName` function that accepts an argument of `student` object, return the student name
 function getStudentName(student) {
-  // write your code here...
+  // students.forEach(() => {
+  //   return student.name;
+  // });
+  return student.name;
 }
-// console.log(getStudentName(students[0]))
+// console.log(getStudentName());
+// write your code here...
+console.log(getStudentName(students[3]));
 
-// 2) Write a `getCourse` function that accepts a `student` object and `courseIndex` return the course at the specified course index in the student's courses array
+// 2) Write a `getCourse` function that accepts a `student` object and `courseIndex`
+// return the course at the specified course index in the student's courses array
 function getCourse(student, courseIndex) {
-  // write your code here...
-}
-// console.log(getCourse(students[4], 2)); // Outputs: Music
+  return student.courses[courseIndex];
+  // return student.courses[1];
+} //bring back course name when calling its index from the array
+//so we need the index of the student then we want to check his courses
+// write your code here...
+console.log(getCourse(students[4], 2)); // Outputs: Music
 
 // 3) Write a `addCourseToStudent` function that accepts a `student` object and `course` string,
 // it will add the course to the student's courses array and return the `student` object
 function addCourseToStudent(student, course) {
-  // write your code here...
+  student.courses.push(course);
+  return student;
 }
-// console.log(addCourseToStudent(students[7], "Physics"));
+// write your code here...
+console.log(addCourseToStudent(students[7], "Physics"));
 
 // 4) Write a `countCourses` function that accepts a `student` object
 // then returns the number of courses the student is enrolled in
 function countCourses(student) {
+  return student.courses.length;
   // write your code here...
 }
-// console.log(countCourses(students[1])); // Outputs: 4
+console.log(countCourses(students[1])); // Outputs: 4
 
 // 5) Write a `removeCourseFromStudent` function that accepts a `student` object and `course` string,
 // removes the `course` from the student's courses array,
 // then returns the `student` object.
+// id: 10,
+// name: "Jack",
+// courses: ["Math", "History", "Music"],
 function removeCourseFromStudent(student, course) {
-  // write your code here...
+  // console.log(student.courses.slice());
 }
-// console.log(removeCourseFromStudent(students[6], "Science"));
+// write your code here...
+console.log(removeCourseFromStudent(students[6], "Science"));
 
 // 6) Write a `findStudentById` function that accepts a `studentId` and an array of student objects `students`
 // and returns the student object with the matching id.
 // It should return undefinded if a student is not found
 function findStudentById(students, studentId) {
-  // write your code here...
+  // if (students.id == studentId) {
+  //   return studentId;
+  // } else {
+  //   return "undefined";
+  // }
+  return students.find((student) => student.id == studentId);
 }
-// console.log(findStudentById(students, 10));
+
+console.log(findStudentById(students, 10));
+
+//this will return an array (filter)
+// let x = students.filter((student) => {
+//   return student.id == 5;
+// });
+// console.log(x);
 
 // 🌶️🌶️
 // 7) Write a `getStudentsByCourse` function that accepts a `course` string and an array of student objects `students`,
